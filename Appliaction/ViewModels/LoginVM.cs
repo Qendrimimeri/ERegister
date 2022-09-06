98f9f5a61abb945092425e32b43ccee5df1400e1
Arbnora Veliqi <veliqiarbenore@gmail.com>
2022-09-06 12:58:14 +0200

    Login Backend

---
+++ Appliaction/ViewModels/LoginVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.ViewModels
+{
+    public class LoginVM
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        //[Required, MinLength(8, ErrorMessage = "Password should be more than 8 caharacters")]
+        public string Password { get; set; }
+    }
+}
