f88b4c8c390059589a2df9532ba23d0abf1cc492
Bledian <bledianpotera1@gmail.com>
2022-09-20 14:27:39 +0200

    final one

---
+++ Appliaction/ViewModels/KqzRegisterVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.ViewModels
+{
+    public class KqzRegisterVM
+    {
+        public int Id { get; set; }
+        public int? PoliticialSubjectId { get; set; }
+        public int? NoOfvotes { get; set; }
+        public int? PollCenterId { get; set; }
+        public string? DataCreated { get; set; }
+        public int? MunicipalityId { get; set; }
+        public int? VillageId { get; set; }
+        public int? NeighborhoodId { get; set; }
+        public string? ElectionType { get; set; }
+    }
+}
