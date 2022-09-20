5b5bf7a3eba5b344818d7675421847dc029231d7
Zana <zanaashehu@gmail.com>
2022-09-20 09:57:13 +0200

    Db changes on PollRelated Table

---
+++ Domain/Migrations/20220920070644_test.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domain.Migrations
+{
+    public partial class test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "GeneralDescription",
+                table: "PollRelateds",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "PollRelateds",
+                keyColumn: "GeneralDescription",
+                keyValue: null,
+                column: "GeneralDescription",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "GeneralDescription",
+                table: "PollRelateds",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
