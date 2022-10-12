00b69b0cb8ae033a964de48b120bdcb3d5b87331
Bledian <bledianpotera1@gmail.com>
2022-10-12 13:46:19 +0200

    ca ndryshime

--- Appliaction/ViewModels/VoterDetailsVM.cs
+++ Appliaction/ViewModels/VoterDetailsVM.cs
@@ -1,66 +1,64 @@
-﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
-
-namespace Application.ViewModels
+﻿namespace Application.ViewModels
 {
     public class VoterDetailsVM : RegisterVM
     {
-        [ValidateNever]
+        
         public string Id { get; set; }
 
-        [ValidateNever]
+     
         public string? Neigborhood { get; set; }
 
-        [ValidateNever]
+     
         public string? Village { get; set; }
 
-        [ValidateNever]
+        
         public string? Block { get; set; }
 
 
-        [ValidateNever]
+        
         public string? GeneralReason { get; set; }
 
 
-        [ValidateNever]
+        
         public string? MunicipalityName { get; set; }
 
-        [ValidateNever]
+       
         public string? SpecificReason { get; set; }
 
-        [ValidateNever]
+       
         public int? CanYouManage { get; set; }
 
-        [ValidateNever]
+       
         public string? ActivitiesYourPlan { get; set; }
 
-        [ValidateNever]
+        
         public int? NeedHelp { get; set; }
 
-        [ValidateNever]
+ 
         public string? SpecificDemand { get; set; }
 
-        [ValidateNever]
+
         public string? GeneralDemands { get; set; }
 
-        [ValidateNever]
+        
         public string? GeneralDescription { get; set; }
 
-        [ValidateNever]
+       
         public string? PoliticalSubjects { get; set; }
 
-        [ValidateNever]
+        
         public string? InitialChance { get; set; }
 
-        [ValidateNever]
+     
         public string? CurrentVoter { get; set; }
 
-        [ValidateNever]
+
         public string? PreviousVoter { get; set; }
 
-        [ValidateNever]
+      
         public int? VotersNumber { get; set; }
 
-        [ValidateNever]
+       
         public string? FacebookLink { get; set; }
         //public int? FamMembersNumber { get; set; }
     }
