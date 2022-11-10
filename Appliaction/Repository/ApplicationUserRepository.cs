9c72e3157a727df783613399e94e698d8df33d4f
Muhamet <behramimuhamet@gmail.com>
2022-11-10 11:22:07 +0100

    ndryshimet

--- Appliaction/Repository/ApplicationUserRepository.cs
+++ Appliaction/Repository/ApplicationUserRepository.cs
@@ -182,7 +182,7 @@ namespace Application.Repository
                     PhoneNumber = encrypt.Decrypt(person.PhoneNumber),
 
                     Email = person.Email,
-                    FacebookLink = person.SocialNetwork,
+                    FacebookLink =  FacebookUserName(person.SocialNetwork),
                     WorkPlace = person.Work.WorkPlace,
                     AdministrativeUnit = person.Work.AdministrativeUnit,
                     Duty = person.Work.Duty,
@@ -212,7 +212,7 @@ namespace Application.Repository
                         HouseNo = person.Address.HouseNo,
                         PhoneNumber = encrypt.Decrypt(person.PhoneNumber),
                         Email = person.Email,
-                        FacebookLink = person.SocialNetwork,
+                        FacebookLink = FacebookUserName(person.SocialNetwork),
                         WorkPlace = person.Work.WorkPlace,
                         AdministrativeUnit = person.Work.AdministrativeUnit,
                         Duty = person.Work.Duty,
@@ -587,7 +587,18 @@ namespace Application.Repository
 
         public async Task<bool> CheckUser(string email, string password) =>
             await _userManager.CheckPasswordAsync(await _userManager.FindByEmailAsync(email), password);
-            
+
+        public string FacebookUserName(string url)
+        {
+            var foundIndexes = new List<int>();
+
+            for (int i = url.IndexOf('/'); i > -1; i = url.IndexOf('/', i + 1))
+            {
+                foundIndexes.Add(i);
+            }
+            var res = url.Substring(foundIndexes[2] + 1);
+            return res;
+        }
     }
 
 #pragma warning restore CS8604 
