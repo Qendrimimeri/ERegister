b96575b8a7489b8be8551a0eea1749c87c64b4fe
Qendrim imeri <qendrim.s.imeri@gmail.com>
2022-12-16 08:53:09 +0100

    vm

---
+++ Appliaction/ViewModels/VoterVM.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.ViewModels;
+
+public class VoterVM
+{
+    public string? Id { get; set; }
+    public string? FullName { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? MunicipalityName { get; set; }
+    public string? PollCenter { get; set; }
+    public int? VotersNumber { get; set; }
+    public string? PreviousVoter { get; set; }
+
+    public string? CurrentVoter { get; set; }
+
+    //Gjasat Fillestare
+    public string? InitialChances { get; set; }
+
+    //Gjasat Aktuale .
+    public string? ActualChances { get; set; }
+
+    public string? ActualStatus { get; set; }
+
+    //public string PoliticalSubject { get; set; }
+    public string? Description { get; set; }
+
+    public string? Village { get; set; }
+}
