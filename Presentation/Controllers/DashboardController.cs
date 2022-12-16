b6ffae699c371f0b6b916c2c624d186fceec1fd4
Muhamet <behramimuhamet@gmail.com>
2022-12-16 09:40:45 +0100

    performance

--- Presentation/Controllers/DashboardController.cs
+++ Presentation/Controllers/DashboardController.cs
@@ -78,7 +78,22 @@ namespace Presentation.Controllers
 
 
         [HttpGet, Authorize(Roles = "KryetarIPartise,KryetarIKomunes,KryetarIFshatit")]
-        public async Task<IActionResult> Performance()
+        public async Task<IActionResult> PerformanceLocal()
+        {
+            try
+            {
+                var voters = await _unitOfWork.ApplicationUser.GetPersonInfoAsync();
+                ViewBag.SaveAndCloseManage = TempData["SaveAndCloseManage"] as string;
+                return View(voters);
+            }
+            catch (Exception err)
+            {
+                _logger.LogError("An error has occured", err);
+                return View(errorView);
+            }
+        }
+        [HttpGet, Authorize(Roles = "KryetarIPartise,KryetarIKomunes,KryetarIFshatit")]
+        public async Task<IActionResult> PerformanceNational()
         {
             try
             {
