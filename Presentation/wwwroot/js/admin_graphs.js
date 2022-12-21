d9c75ad4dcab9de49981cce50e074a1d803b1b89
Qendrim imeri <qendrim.s.imeri@gmail.com>
2022-12-21 11:25:43 +0100

    perfundimi me grafe

--- Presentation/wwwroot/js/admin_graphs.js
+++ Presentation/wwwroot/js/admin_graphs.js
@@ -14,20 +14,32 @@ async function getId() {
         data.push(x)
     });
 
-    const nacionaleData = data[select - 1].value.Lokale["thisYear"];
+
     const lastYear = data[select - 1].value.Nacionale["lastYear"];
     const thisYear = data[select - 1].value.Nacionale["thisYear"];
+
+    const lastYearLocal = data[select - 1].value.Lokale["lastYear"];
+    const thisYearLocal = data[select - 1].value.Lokale["thisYear"];
+
     const politicalSubjects = data[select - 1].value.Nacionale["politicSubjects"];
 
     document.getElementById("city-nacinale").innerText = `${data[select - 1].key} - Zgjedhjet Nacionale`;
     document.getElementById("city-lokale").innerText = `${data[select - 1].key} - Zgjedhjet Lokale`;
-    let lokale = Object.keys(lastYear).length;
-    let nacionale = Object.keys(nacionaleData).length
 
-    console.log(lokale)
-    console.log(nacionale)
+    let nLastYear = Object.keys(lastYear).length;
+    let nThisYear = Object.keys(thisYear).length
 
-    if (lokale <= 0 && nacionale <= 0) {
+    let localLastYear = Object.keys(lastYearLocal).length;
+    let localThisYear = Object.keys(thisYearLocal).length;
+
+    console.log("nacionale last year" + nLastYear)
+    console.log("nacionale this year" + nThisYear)
+
+    console.log("local last year" + localLastYear)
+    console.log("local this year" + localThisYear)
+
+
+    if (nLastYear <= 0 && nThisYear <= 0) {
         const nacionaleOptions = {
             series: [{
                 name: 'Viti 2021',
@@ -81,10 +93,12 @@ async function getId() {
         this.nacionaleChart = await new ApexCharts(document.querySelector("#columnChart"), nacionaleOptions);
         this.nacionaleChart.render()
 
-        const lokaleOptions = {
+    }
+    else if (nThisYear <= 0 && nLastYear > 0) {
+        const nacionaleOptions = {
             series: [{
                 name: 'Viti 2021',
-                data: [0, 0, 0, 0, 0, 0, 0, 0]
+                data: [lastYear[politicalSubjects[0]], lastYear[politicalSubjects[1]], lastYear[politicalSubjects[2]], lastYear[politicalSubjects[3]], lastYear[politicalSubjects[4]], lastYear[politicalSubjects[5]], lastYear[politicalSubjects[6]], lastYear[politicalSubjects[7]]]
             }, {
                 name: 'Tani',
                 data: [0, 0, 0, 0, 0, 0, 0, 0]
@@ -113,7 +127,7 @@ async function getId() {
                 colors: ['transparent']
             },
             xaxis: {
-                categories: politicalSubjects
+                categories: politicalSubjects,
             },
             fill: {
                 opacity: 1,
@@ -127,18 +141,18 @@ async function getId() {
                 }
             }
         }
-        if (this.lokaleChart)
-            this.lokaleChart.destroy();
 
-        this.lokaleChart = await new ApexCharts(document.querySelector("#columnChart1"), lokaleOptions);
-        this.lokaleChart.render()
+        if (this.nacionaleChart)
+            this.nacionaleChart.destroy();
 
+        this.nacionaleChart = await new ApexCharts(document.querySelector("#columnChart"), nacionaleOptions);
+        this.nacionaleChart.render()
     }
-    else if (lokale <= 0) {
+    else if (nThisYear > 0 && nLastYear <= 0) {
         const nacionaleOptions = {
             series: [{
                 name: 'Viti 2021',
-                data: [lastYear[politicalSubjects[0]], lastYear[politicalSubjects[1]], lastYear[politicalSubjects[2]], lastYear[politicalSubjects[3]], lastYear[politicalSubjects[4]], lastYear[politicalSubjects[5]], lastYear[politicalSubjects[6]], lastYear[politicalSubjects[7]]]
+                data: [0, 0, 0, 0, 0, 0, 0, 0]
             }, {
                 name: 'Tani',
                 data: [thisYear[politicalSubjects[0]], thisYear[politicalSubjects[1]], thisYear[politicalSubjects[2]], thisYear[politicalSubjects[3]], thisYear[politicalSubjects[4]], thisYear[politicalSubjects[5]], thisYear[politicalSubjects[6]], thisYear[politicalSubjects[7]]]
@@ -187,167 +201,6 @@ async function getId() {
 
         this.nacionaleChart = await new ApexCharts(document.querySelector("#columnChart"), nacionaleOptions);
         this.nacionaleChart.render()
-        const lokaleOptions = {
-            series: [{
-                name: 'Viti 2021',
-                data: [0, 0, 0, 0, 0, 0, 0, 0]
-            }, {
-                name: 'Tani',
-                data: [0, 0, 0, 0, 0, 0, 0, 0]
-            }],
-            chart: {
-                type: 'bar',
-                height: 350
-            }, colors: ['#339af0', '#f76707'],
-            stroke: {
-                curve: 'smooth',
-                width: 0.5,
-            },
-            plotOptions: {
-                bar: {
-                    horizontal: false,
-                    columnWidth: '55%',
-                    endingShape: 'rounded'
-                },
-            },
-            dataLabels: {
-                enabled: false
-            },
-            stroke: {
-                show: true,
-                width: 2,
-                colors: ['transparent']
-            },
-            xaxis: {
-                categories: politicalSubjects
-            },
-            fill: {
-                opacity: 1,
-                colors: ['#339af0', '#f76707']
-            },
-            tooltip: {
-                y: {
-                    formatter: function (val) {
-                        return " " + val + " vota"
-                    }
-                }
-            }
-        }
-        if (this.lokaleChart)
-            this.lokaleChart.destroy();
-
-        this.lokaleChart = await new ApexCharts(document.querySelector("#columnChart1"), lokaleOptions);
-        this.lokaleChart.render();
-
-    }
-    else if (nacionale <= 0) {
-        const nacionaleOptions = {
-            series: [{
-                name: 'Viti 2021',
-                data: [0, 0, 0, 0, 0, 0, 0, 0]
-            }, {
-                name: 'Tani',
-                data: [0, 0, 0, 0, 0, 0, 0, 0]
-            }],
-            chart: {
-                type: 'bar',
-                height: 350
-            }, colors: ['#339af0', '#f76707'],
-            stroke: {
-                curve: 'smooth',
-                width: 0.5,
-            },
-            plotOptions: {
-                bar: {
-                    horizontal: false,
-                    columnWidth: '55%',
-                    endingShape: 'rounded'
-                },
-            },
-            dataLabels: {
-                enabled: false
-            },
-            stroke: {
-                show: true,
-                width: 2,
-                colors: ['transparent']
-            },
-            xaxis: {
-                categories: politicalSubjects,
-            },
-            fill: {
-                opacity: 1,
-                colors: ['#339af0', '#f76707']
-            },
-            tooltip: {
-                y: {
-                    formatter: function (val) {
-                        return " " + val + " vota"
-                    }
-                }
-            }
-        }
-
-        if (this.nacionaleChart)
-            this.nacionaleChart.destroy();
-
-        this.nacionaleChart = await new ApexCharts(document.querySelector("#columnChart"), nacionaleOptions);
-        this.nacionaleChart.render()
-
-        const lastYearLocal = data[select - 1].value.Lokale["lastYear"]
-        const thisYearLocal = data[select - 1].value.Lokale["thisYear"]
-
-        const lokaleOptions = {
-            series: [{
-                name: 'Viti 2021',
-                data: [lastYearLocal[politicalSubjects[0]], lastYearLocal[politicalSubjects[1]], lastYearLocal[politicalSubjects[2]], lastYearLocal[politicalSubjects[3]], lastYearLocal[politicalSubjects[4]], lastYearLocal[politicalSubjects[5]], lastYearLocal[politicalSubjects[6]], lastYearLocal[politicalSubjects[7]]]
-            }, {
-                name: 'Tani',
-                data: [thisYearLocal[politicalSubjects[0]], thisYearLocal[politicalSubjects[1]], thisYearLocal[politicalSubjects[2]], thisYearLocal[politicalSubjects[3]], thisYearLocal[politicalSubjects[4]], thisYearLocal[politicalSubjects[5]], thisYearLocal[politicalSubjects[6]], thisYearLocal[politicalSubjects[7]]]
-            }],
-            chart: {
-                type: 'bar',
-                height: 350
-            }, colors: ['#339af0', '#f76707'],
-            stroke: {
-                curve: 'smooth',
-                width: 0.5,
-            },
-            plotOptions: {
-                bar: {
-                    horizontal: false,
-                    columnWidth: '55%',
-                    endingShape: 'rounded'
-                },
-            },
-            dataLabels: {
-                enabled: false
-            },
-            stroke: {
-                show: true,
-                width: 2,
-                colors: ['transparent']
-            },
-            xaxis: {
-                categories: politicalSubjects
-            },
-            fill: {
-                opacity: 1,
-                colors: ['#339af0', '#f76707']
-            },
-            tooltip: {
-                y: {
-                    formatter: function (val) {
-                        return " " + val + " vota"
-                    }
-                }
-            }
-        }
-        if (this.lokaleChart)
-            this.lokaleChart.destroy();
-
-        this.lokaleChart = await new ApexCharts(document.querySelector("#columnChart1"), lokaleOptions);
-        this.lokaleChart.render()
     }
     else {
         const nacionaleOptions = {
@@ -402,10 +255,169 @@ async function getId() {
 
         this.nacionaleChart = await new ApexCharts(document.querySelector("#columnChart"), nacionaleOptions);
         this.nacionaleChart.render()
+    }
 
-        const lastYearLocal = data[select - 1].value.Lokale["lastYear"]
-        const thisYearLocal = data[select - 1].value.Lokale["thisYear"]
 
+    if (localLastYear <= 0 && localThisYear <= 0) {
+        const lokaleOptions = {
+            series: [{
+                name: 'Viti 2021',
+                data: [0, 0, 0, 0, 0, 0, 0, 0]
+            }, {
+                name: 'Tani',
+                data: [0, 0, 0, 0, 0, 0, 0, 0]
+            }],
+            chart: {
+                type: 'bar',
+                height: 350
+            }, colors: ['#339af0', '#f76707'],
+            stroke: {
+                curve: 'smooth',
+                width: 0.5,
+            },
+            plotOptions: {
+                bar: {
+                    horizontal: false,
+                    columnWidth: '55%',
+                    endingShape: 'rounded'
+                },
+            },
+            dataLabels: {
+                enabled: false
+            },
+            stroke: {
+                show: true,
+                width: 2,
+                colors: ['transparent']
+            },
+            xaxis: {
+                categories: politicalSubjects
+            },
+            fill: {
+                opacity: 1,
+                colors: ['#339af0', '#f76707']
+            },
+            tooltip: {
+                y: {
+                    formatter: function (val) {
+                        return " " + val + " vota"
+                    }
+                }
+            }
+        }
+        if (this.lokaleChart)
+            this.lokaleChart.destroy();
+
+        this.lokaleChart = await new ApexCharts(document.querySelector("#columnChart1"), lokaleOptions);
+        this.lokaleChart.render()
+    }
+    else if (localThisYear <= 0 && localLastYear > 0) {
+        const lokaleOptions = {
+            series: [{
+                name: 'Viti 2021',
+                data: [lastYearLocal[politicalSubjects[0]], lastYearLocal[politicalSubjects[1]], lastYearLocal[politicalSubjects[2]], lastYearLocal[politicalSubjects[3]], lastYearLocal[politicalSubjects[4]], lastYearLocal[politicalSubjects[5]], lastYearLocal[politicalSubjects[6]], lastYearLocal[politicalSubjects[7]]]
+            }, {
+                name: 'Tani',
+                data: [0, 0, 0, 0, 0, 0, 0, 0]
+            }],
+            chart: {
+                type: 'bar',
+                height: 350
+            }, colors: ['#339af0', '#f76707'],
+            stroke: {
+                curve: 'smooth',
+                width: 0.5,
+            },
+            plotOptions: {
+                bar: {
+                    horizontal: false,
+                    columnWidth: '55%',
+                    endingShape: 'rounded'
+                },
+            },
+            dataLabels: {
+                enabled: false
+            },
+            stroke: {
+                show: true,
+                width: 2,
+                colors: ['transparent']
+            },
+            xaxis: {
+                categories: politicalSubjects
+            },
+            fill: {
+                opacity: 1,
+                colors: ['#339af0', '#f76707']
+            },
+            tooltip: {
+                y: {
+                    formatter: function (val) {
+                        return " " + val + " vota"
+                    }
+                }
+            }
+        }
+        if (this.lokaleChart)
+            this.lokaleChart.destroy();
+
+        this.lokaleChart = await new ApexCharts(document.querySelector("#columnChart1"), lokaleOptions);
+        this.lokaleChart.render()
+    }
+    else if (localThisYear > 0 && localLastYear <= 0) {
+        const lokaleOptions = {
+            series: [{
+                name: 'Viti 2021',
+                data: [0, 0, 0, 0, 0, 0, 0, 0]
+            }, {
+                name: 'Tani',
+                data: [thisYearLocal[politicalSubjects[0]], thisYearLocal[politicalSubjects[1]], thisYearLocal[politicalSubjects[2]], thisYearLocal[politicalSubjects[3]], thisYearLocal[politicalSubjects[4]], thisYearLocal[politicalSubjects[5]], thisYearLocal[politicalSubjects[6]], thisYearLocal[politicalSubjects[7]]]
+            }],
+            chart: {
+                type: 'bar',
+                height: 350
+            }, colors: ['#339af0', '#f76707'],
+            stroke: {
+                curve: 'smooth',
+                width: 0.5,
+            },
+            plotOptions: {
+                bar: {
+                    horizontal: false,
+                    columnWidth: '55%',
+                    endingShape: 'rounded'
+                },
+            },
+            dataLabels: {
+                enabled: false
+            },
+            stroke: {
+                show: true,
+                width: 2,
+                colors: ['transparent']
+            },
+            xaxis: {
+                categories: politicalSubjects
+            },
+            fill: {
+                opacity: 1,
+                colors: ['#339af0', '#f76707']
+            },
+            tooltip: {
+                y: {
+                    formatter: function (val) {
+                        return " " + val + " vota"
+                    }
+                }
+            }
+        }
+        if (this.lokaleChart)
+            this.lokaleChart.destroy();
+
+        this.lokaleChart = await new ApexCharts(document.querySelector("#columnChart1"), lokaleOptions);
+        this.lokaleChart.render()
+    }
+    else {
         const lokaleOptions = {
             series: [{
                 name: 'Viti 2021',
--- Presentation/wwwroot/js/muni_graphs.js
+++ Presentation/wwwroot/js/muni_graphs.js
@@ -1,19 +1,33 @@
 ﻿const kqzrez = "/api/service/kqzresultsbymuni";
 let response = fetch(kqzrez).then(res => res.json());
-let rez = response.then((values) => {
+let rez = response.then((data) => {
+    document.getElementById("city-nacinale").innerText = `${data.Nacionale.cityName} - Zgjedhjet Nacionale`;
+    document.getElementById("city-lokale").innerText = `${data.Nacionale.cityName} - Zgjedhjet Lokale`;
 
-    const nacionaleData = values.Lokale["lastYear"];
-    const lastYear = values.Nacionale["lastYear"];
-    const thisYear = values.Nacionale["thisYear"];
-    const cityName = values.Nacionale["cityName"];
-    const politicalSubjects = values.Nacionale["politicSubjects"];
-    document.getElementById("city-nacinale").innerText = `${cityName} - Zgjedhjet Nacionale`;
-    document.getElementById("city-lokale").innerText = `${cityName} - Zgjedhjet Lokale`;
+    console.log(data)
 
-    let lokale = Object.keys(lastYear).length;
-    let nacionale = Object.keys(nacionaleData).length
+    const lastYear = data.Nacionale["lastYear"];
+    const thisYear = data.Nacionale["thisYear"];
 
-    if (lokale <= 0 && nacionale <= 0) {
+    const lastYearLocal = data.Lokale["lastYear"];
+    const thisYearLocal = data.Lokale["thisYear"];
+
+    const politicalSubjects = data.Nacionale["politicSubjects"];
+
+    let nLastYear = Object.keys(lastYear).length;
+    let nThisYear = Object.keys(thisYear).length
+
+    let localLastYear = Object.keys(lastYearLocal).length;
+    let localThisYear = Object.keys(thisYearLocal).length;
+
+    console.log("nacionale last year" + nLastYear)
+    console.log("nacionale this year" + nThisYear)
+
+    console.log("local last year" + localLastYear)
+    console.log("local this year" + localThisYear)
+
+
+    if (nLastYear <= 0 && nThisYear <= 0) {
         new ApexCharts(document.querySelector("#columnChart"), {
             series: [{
                 name: 'Viti 2021',
@@ -59,15 +73,65 @@ let rez = response.then((values) => {
                     }
                 }
             }
-        }).render();
-
-        new ApexCharts(document.querySelector("#columnChart1"), {
+        }).render()
+    }
+    else if (nThisYear <= 0 && nLastYear > 0) {
+        new ApexCharts(document.querySelector("#columnChart"),
+            {
+                series: [{
+                    name: 'Viti 2021',
+                    data: [lastYear[politicalSubjects[0]], lastYear[politicalSubjects[1]], lastYear[politicalSubjects[2]], lastYear[politicalSubjects[3]], lastYear[politicalSubjects[4]], lastYear[politicalSubjects[5]], lastYear[politicalSubjects[6]], lastYear[politicalSubjects[7]]]
+                }, {
+                    name: 'Tani',
+                    data: [0, 0, 0, 0, 0, 0, 0, 0]
+                }],
+                chart: {
+                    type: 'bar',
+                    height: 350
+                }, colors: ['#339af0', '#f76707'],
+                stroke: {
+                    curve: 'smooth',
+                    width: 0.5,
+                },
+                plotOptions: {
+                    bar: {
+                        horizontal: false,
+                        columnWidth: '55%',
+                        endingShape: 'rounded'
+                    },
+                },
+                dataLabels: {
+                    enabled: false
+                },
+                stroke: {
+                    show: true,
+                    width: 2,
+                    colors: ['transparent']
+                },
+                xaxis: {
+                    categories: politicalSubjects,
+                },
+                fill: {
+                    opacity: 1,
+                    colors: ['#339af0', '#f76707']
+                },
+                tooltip: {
+                    y: {
+                        formatter: function (val) {
+                            return " " + val + " vota"
+                        }
+                    }
+                }
+            }).render();
+    }
+    else if (nThisYear > 0 && nLastYear <= 0) {
+        new ApexCharts(document.querySelector("#columnChart"), {
             series: [{
                 name: 'Viti 2021',
                 data: [0, 0, 0, 0, 0, 0, 0, 0]
             }, {
                 name: 'Tani',
-                data: [0, 0, 0, 0, 0, 0, 0, 0]
+                data: [thisYear[politicalSubjects[0]], thisYear[politicalSubjects[1]], thisYear[politicalSubjects[2]], thisYear[politicalSubjects[3]], thisYear[politicalSubjects[4]], thisYear[politicalSubjects[5]], thisYear[politicalSubjects[6]], thisYear[politicalSubjects[7]]]
             }],
             chart: {
                 type: 'bar',
@@ -93,7 +157,7 @@ let rez = response.then((values) => {
                 colors: ['transparent']
             },
             xaxis: {
-                categories: politicalSubjects
+                categories: politicalSubjects,
             },
             fill: {
                 opacity: 1,
@@ -155,9 +219,155 @@ let rez = response.then((values) => {
                 }
             }
         }).render();
+    }
 
-        const lastYearLocal = values.Lokale["lastYear"]
-        const thisYearLocal = values.Lokale["thisYear"]
+
+
+    if (localLastYear <= 0 && localThisYear <= 0) {
+        new ApexCharts(document.querySelector("#columnChart1"), {
+            series: [{
+                name: 'Viti 2021',
+                data: [0, 0, 0, 0, 0, 0, 0, 0]
+            }, {
+                name: 'Tani',
+                data: [0, 0, 0, 0, 0, 0, 0, 0]
+            }],
+            chart: {
+                type: 'bar',
+                height: 350
+            }, colors: ['#339af0', '#f76707'],
+            stroke: {
+                curve: 'smooth',
+                width: 0.5,
+            },
+            plotOptions: {
+                bar: {
+                    horizontal: false,
+                    columnWidth: '55%',
+                    endingShape: 'rounded'
+                },
+            },
+            dataLabels: {
+                enabled: false
+            },
+            stroke: {
+                show: true,
+                width: 2,
+                colors: ['transparent']
+            },
+            xaxis: {
+                categories: politicalSubjects
+            },
+            fill: {
+                opacity: 1,
+                colors: ['#339af0', '#f76707']
+            },
+            tooltip: {
+                y: {
+                    formatter: function (val) {
+                        return " " + val + " vota"
+                    }
+                }
+            }
+        }).render();
+    }
+    else if (localThisYear <= 0 && localLastYear > 0) {
+        new ApexCharts(document.querySelector("#columnChart1"), {
+            series: [{
+                name: 'Viti 2021',
+                data: [lastYearLocal[politicalSubjects[0]], lastYearLocal[politicalSubjects[1]], lastYearLocal[politicalSubjects[2]], lastYearLocal[politicalSubjects[3]], lastYearLocal[politicalSubjects[4]], lastYearLocal[politicalSubjects[5]], lastYearLocal[politicalSubjects[6]], lastYearLocal[politicalSubjects[7]]]
+            }, {
+                name: 'Tani',
+                data: [0, 0, 0, 0, 0, 0, 0, 0]
+            }],
+            chart: {
+                type: 'bar',
+                height: 350
+            }, colors: ['#339af0', '#f76707'],
+            stroke: {
+                curve: 'smooth',
+                width: 0.5,
+            },
+            plotOptions: {
+                bar: {
+                    horizontal: false,
+                    columnWidth: '55%',
+                    endingShape: 'rounded'
+                },
+            },
+            dataLabels: {
+                enabled: false
+            },
+            stroke: {
+                show: true,
+                width: 2,
+                colors: ['transparent']
+            },
+            xaxis: {
+                categories: politicalSubjects
+            },
+            fill: {
+                opacity: 1,
+                colors: ['#339af0', '#f76707']
+            },
+            tooltip: {
+                y: {
+                    formatter: function (val) {
+                        return " " + val + " vota"
+                    }
+                }
+            }
+        }).render();
+    }
+    else if (localThisYear > 0 && localLastYear <= 0) {
+        new ApexCharts(document.querySelector("#columnChart1"), {
+            series: [{
+                name: 'Viti 2021',
+                data: [0, 0, 0, 0, 0, 0, 0, 0]
+            }, {
+                name: 'Tani',
+                data: [thisYearLocal[politicalSubjects[0]], thisYearLocal[politicalSubjects[1]], thisYearLocal[politicalSubjects[2]], thisYearLocal[politicalSubjects[3]], thisYearLocal[politicalSubjects[4]], thisYearLocal[politicalSubjects[5]], thisYearLocal[politicalSubjects[6]], thisYearLocal[politicalSubjects[7]]]
+            }],
+            chart: {
+                type: 'bar',
+                height: 350
+            }, colors: ['#339af0', '#f76707'],
+            stroke: {
+                curve: 'smooth',
+                width: 0.5,
+            },
+            plotOptions: {
+                bar: {
+                    horizontal: false,
+                    columnWidth: '55%',
+                    endingShape: 'rounded'
+                },
+            },
+            dataLabels: {
+                enabled: false
+            },
+            stroke: {
+                show: true,
+                width: 2,
+                colors: ['transparent']
+            },
+            xaxis: {
+                categories: politicalSubjects
+            },
+            fill: {
+                opacity: 1,
+                colors: ['#339af0', '#f76707']
+            },
+            tooltip: {
+                y: {
+                    formatter: function (val) {
+                        return " " + val + " vota"
+                    }
+                }
+            }
+        }).render();
+    }
+    else {
         new ApexCharts(document.querySelector("#columnChart1"), {
             series: [{
                 name: 'Viti 2021',
